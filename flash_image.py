8020973b811c3ab5eee9013173eed426fa62c2d8
Daniel E. Moser <office@mojambo.at>
2018-02-12 20:34:01 +0100

    basic functionality implemented

--- flash_image.py
+++ flash_image.py
@@ -7,56 +7,149 @@ import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 conf_file = os.path.join(dir_path, 'config.ini')
+dev_null = open(os.devnull, 'w')
 
-
-def main():
-    """Main funtion
+def initArgParse():
+    """Parse cmd-line arguments and return the parser
     """
-    # configuragtion parsing
-    config = configparser.ConfigParser()
-    config.read(conf_file)
-
-    sys_part_size = config['DEFAULT']['SystemPartitionSize']
-
-
     # cmd line argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument('dev', action='store',
                         help='device location of sd card')
 
-    parser.add_argument('-a','--agent',  action='store',                        
-                        help='specify the agent type [aMussel | aFish]')
+    parser.add_argument('-if','--is_fish',  action='store_true',                
+                        help='flashed sd is for aFish')
+
     parser.add_argument('-n','--num',  action='store',                    
                         help='specify the number for the generated image for aMussel | aFish')
+
     parser.add_argument('-f', '--force', action='store_true',
                         help='no comfirmations required during run')
+
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='detailed output')
-    args = parser.parse_args()
-    dev = args.dev
 
-    print("*aMuFiFlash*")
-    print("$$$")
-    
-    print("Flashing device {} with {} MB system- and <device-size> -{} MB data partition.".format(dev.upper(), sys_part_size, sys_part_size))
+    parser.add_argument('-p', '--partition_only', action='store_true',
+                        help='only do the partition')
+   
+    parser.add_argument('-i', '--image_only', action='store_true',
+                        help='only do the flashing of the image')
 
-    if not args.force:
-        print("Press the <enter> key to continue...")
-        input()
+    return parser.parse_args()
 
+
+def partition(args, conf):
+    """Construct the fdisk partition command and call the partition bash script to execute the command with the given options
+    """
+    sys_part_size = conf['DEFAULT']['SystemPartitionSize']
+
+    print("Partitioning device {}: system - {}MB ; data - <device-size> -{}MB.".format(args.dev, sys_part_size, sys_part_size))
+
+    # create partition command
     partition_script_path = os.path.join(dir_path, 'partition.sh')
-    cmd = [partition_script_path, str(dev),str(sys_part_size)]
+    cmd = [partition_script_path, str(args.dev),str(sys_part_size)]
 
     if args.verbose:
-        print("Command: {}".format(" ".join(cmd)))
+        print("* Executing command: {} ...".format(" ".join(cmd)))
 
+    # execute partition command
     try:
-        output = subprocess.check_output(cmd)
+        subprocess.call(cmd, stdout=dev_null, stderr=dev_null)
     except subprocess.CalledProcessError:
         print("subprocess error while calling '{}'.".format(" ".join(cmd)))
         sys.exit()
 
-    print(str(output))
+    if args.verbose:
+        print(" -->  Formatted {}1 and {}2.\n".format(args.dev,args.dev))
+ 
+
+
+def format(args, conf):
+    """Format partitions 1 and 2 to ext4
+    """
+    dev = args.dev
+    if args.verbose:
+        print("Formatting devices {}1 and {}2 with ext4.".format(dev, dev))
+
+    # create format partition commands
+    cmd1 = ['mkfs.ext4', str('-L'),str('system'),str(dev+'1')]
+
+    cmd2 = ['mkfs.ext4', str('-L'),str('data'),str(dev+'2')]
+    
+    if args.verbose:
+        print("* Executing commands: \n* {} ...\n* {} ...".format(" ".join(cmd1), " ".join(cmd2)))
+    try:
+        subprocess.call(cmd1, stdout=dev_null, stderr=dev_null)
+    except subprocess.CalledProcessError:
+        print("subprocess error while calling '{}'.".format(" ".join(cmd1)))
+        sys.exit()
+
+    try:
+        subprocess.call(cmd2, stdout=dev_null, stderr=dev_null)
+    except subprocess.CalledProcessError:
+        print("subprocess error while calling '{}'.".format(" ".join(cmd2)))
+        sys.exit()
+    
+    if args.verbose:
+        print(" -->  Formatted {}1 to ext4, labeled 'system', {}2 to ext4, labeled 'data'\n".format(dev, dev))
+
+
+
+def flash(args, conf):
+    """ Flash the system partition of the sd with the image specified in 'config.ini' or in the 'image' cmd-line argument. Test if flashing was successful.
+    """
+    img = os.path.join(dir_path, conf['DEFAULT']['Image'])
+    dev = args.dev
+
+    if args.verbose:
+        print("Flashing device {}1 with image {}.".format(dev, img))
+
+    # create flashing command
+    cmd = ['dd', str('if={}'.format(img)), str('of={}1'.format(dev)), str('status=progress'), str('bs=4M')]
+    if args.verbose:
+        print("* Executing command: {} ...".format(" ".join(cmd)))
+
+    # execute command
+    try:
+        subprocess.call(cmd, stdout=dev_null, stderr=dev_null)
+    except subprocess.CalledProcessError:
+        print("subprocess error while calling '{}'.".format(" ".join(cmd)))
+        sys.exit()
+
+    if args.verbose:
+        print(" --> Flashed image '{}' to device {}1\n".format(img, dev))
+
+
+def main():
+    """Main function
+    """    
+    # configuragtion parsing
+    config = configparser.ConfigParser()
+    config.read(conf_file)
+
+    # parsing args
+    args = initArgParse()
+
+    ### program start
+    print("*aMuFiFlash*")
+    print("$$$")
+
+    # partition sd
+    if not args.image_only:
+        partition(args, config)
+
+    # format sd
+    if not args.image_only:
+        format(args, config)
+
+    # flashing of image
+    if not args.partition_only:
+        flash(args, config)
+
+    # naming and numbering
+
+
+
     
 
 if __name__ == "__main__":
--- partition.sh
+++ partition.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: make_partition.sh [DEV] [SIZE OF BOOT IN MB]!"
+    echo "Usage: partition.sh [DEV] [SIZE OF BOOT IN MB]!"
     exit 1
 fi
 
@@ -11,24 +11,19 @@ fi
 # line terminated with a newline to take the fdisk default.
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $1
   o # clear the in memory partition table
-
-  ## boot partition:
   n # new partition
   p # primary partition
-  1 # partition number 1
+  1  # partition number 1
     # default - start at beginning of disk 
   +$2M # create boot parttion (size specified in $2)
   a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-
-  ## data partition
+    # bootable partition is partition 1 
   n # new partition
   p # primary partition
   2 # partion number 2
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
-
   p # print the in-memory partition table
- # w # write the partition table
+  w # write out partition table
   q # and we're done
 EOF
